7bf28f28eabd29c12671639029f6f6b77264ccc1
Mathieu Gauthier <mathieu.gauthier@cossette.com>
2017-09-18 20:45:29 -0400

    page reloader + fids view with logo and last update

---
+++ docroot/themes/yqb/js/modules/page-reloader.js
@@ -0,0 +1,43 @@
+var Drupal = Drupal || {};
+
+var PageReloader = (function ($, Drupal, Bootstrap) {
+
+    var self = {};
+
+    /** ------------------------------
+     * Constructor/Destructor
+     --------------------------------- */
+
+    self.construct = function () {
+        console.log('Page Reloader constructed');
+
+        // Initialize things
+        
+        if($('#vols-et-destinations-horaire-des-vols-arrivees').length){
+            self.initPageReloader();
+        }
+
+        return self;
+    };
+
+    self.destruct = function () {
+        console.log('Page Reloader destructed');
+
+        // Clean up and uninitialize things
+
+    };
+    
+    self.initPageReloader = function(){
+        console.log('Page Reloader inited');
+        
+        var timer = 5*60; // 5 minutes
+        
+        setTimeout(function(){
+            window.location.href = window.location.href;
+        }, timer*1000);
+    };
+
+    
+    // Return class
+    return self.construct();
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap);
--- docroot/themes/yqb/js/router.js
+++ docroot/themes/yqb/js/router.js
@@ -5,20 +5,19 @@ var Router = (function ($, Drupal, Bootstrap) {
     var self = {};
 
     var routes = [
-        { filter: 'body.path-frontpage',                                                            cls: 'Home',            method: 'index' },
-        { filter: 'body.view-page-departures, body.view-page-arrivals',                             cls: 'Schedule',        method: 'index' },
-        { filter: 'body.view-page-destinations',                                                    cls: 'Destinations',    method: 'index' },
-        { filter: 'body.view-page-publications',                                                    cls: 'Publications',    method: 'index' },
-        { filter: 'body[id*="gouvernance"], body[id*="governance"]',                                cls: 'About',           method: 'governance' },
-        { filter: 'body.view-page-check_in',                                                        cls: 'Pages',    method: 'checkIn' },
-        { filter: 'body[id*="histoire"]',                                                           cls: 'Pages',           method: 'history' },
-        { filter: 'body[id*="quebec"]',                                                             cls: 'Pages',           method: 'visit' },
-        { filter: 'body[id*="app-mobile"]',                                                         cls: 'Pages',           method: 'appMobile' },
-        { filter: 'body[id*="mission-vision"]',                                                     cls: 'Pages',           method: 'mission' },
-        { filter: 'body[id*="vestiaire"], body[id*="coat-check"], body[id*="service-demballage"], body[id*="wrap-baggage"]',                cls: 'Pages',           method: 'storage' },
-        { filter: 'body[id*="facture"], body[id*="bill"], body[id*="stationnement"], body[id*="parking"]',            cls: 'Payments',        method: 'index' },
-        
-        { filter: '.slider-gallery-thumbs',                                                                cls: 'SwiperSlider',           method: 'initWithThumbs' }
+        {filter: 'body.path-frontpage', cls: 'Home', method: 'index'},
+        {filter: 'body.view-page-departures, body.view-page-arrivals', cls: 'Schedule', method: 'index'},
+        {filter: 'body.view-page-destinations', cls: 'Destinations', method: 'index'},
+        {filter: 'body.view-page-publications', cls: 'Publications', method: 'index'},
+        {filter: 'body[id*="gouvernance"], body[id*="governance"]', cls: 'About', method: 'governance'},
+        {filter: 'body.view-page-check_in', cls: 'Pages', method: 'checkIn'},
+        {filter: 'body[id*="histoire"]', cls: 'Pages', method: 'history'},
+        {filter: 'body[id*="quebec"]', cls: 'Pages', method: 'visit'},
+        {filter: 'body[id*="app-mobile"]', cls: 'Pages', method: 'appMobile'},
+        {filter: 'body[id*="mission-vision"]', cls: 'Pages', method: 'mission'},
+        {filter: 'body[id*="vestiaire"], body[id*="coat-check"], body[id*="service-demballage"], body[id*="wrap-baggage"]', cls: 'Pages', method: 'storage'},
+        {filter: 'body[id*="facture"], body[id*="bill"], body[id*="stationnement"], body[id*="parking"]', cls: 'Payments', method: 'index'},
+        {filter: '.slider-gallery-thumbs', cls: 'SwiperSlider', method: 'initWithThumbs'}
     ];
 
     self.currentPage = null;
