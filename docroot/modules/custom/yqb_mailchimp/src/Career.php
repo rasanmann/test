8cf910e273616617888ed11bf9c103483881882e
Francis Santerre <francis.santerre@gmail.com>
2019-02-28 11:27:07 -0500

    Update last career id published

--- docroot/modules/custom/yqb_mailchimp/src/Career.php
+++ docroot/modules/custom/yqb_mailchimp/src/Career.php
@@ -24,7 +24,7 @@ class Career implements ContainerInjectionInterface {
     $query = \Drupal::entityQuery('node');
     $query->condition('status', Node::PUBLISHED);
     $query->condition('type', 'career');
-    $query->condition('nid', 717856, '>');
+    $query->condition('nid', 794801, '>');
     $query->condition('field_start_date', date('Y-m-d', \Drupal::time()->getCurrentTime()), '<=');
     $query->condition('field_end_date', date('Y-m-d', \Drupal::time()->getCurrentTime()), '>=');
     $results = $query->execute();
