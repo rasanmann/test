72df725b6794ba820b15a4c715dd66269977c8bd
Francis Santerre <francis.santerre@gmail.com>
2019-03-12 16:06:44 -0400

    Fix bills block to use controller form

--- docroot/modules/custom/yqb_bills/src/Plugin/Block/BillsBlock.php
+++ docroot/modules/custom/yqb_bills/src/Plugin/Block/BillsBlock.php
@@ -25,9 +25,74 @@ class BillsBlock extends BlockBase implements BlockPluginInterface{
    * {@inheritdoc}
    */
   public function build() {
-      $form = BillsPaymentForm::create(\Drupal::getContainer());
-      $formState = new FormState();
-      $formState->set('form_is_enabled', isset($config['form_is_enabled']) ? 1 : 0);
+    $psStoreId = [
+      '#type' => 'hidden',
+      '#name' => 'ps_store_id',
+      '#value' => 'HEJGN34601'
+
+    ];
+
+    $hppKey = [
+      '#type' => 'hidden',
+      '#name' => 'hpp_key',
+      '#value' => 'hpOUJB91ZLFF'
+
+    ];
+
+    $lang = [
+      '#type' => 'hidden',
+      '#name' => 'lang',
+      '#value' => \Drupal::languageManager()->getCurrentLanguage()->getId() . '-ca'
+    ];
+
+    $fields = [
+      'bill_last_name' => $this->t("Nom"),
+      'bill_first_name' => $this->t("Prénom"),
+      'email' => $this->t("Courriel"),
+      'bill_company_name' => $this->t("Nom de votre entreprise"),
+      'order_id' => $this->t("Numéro de facture"),
+      'cust_id' => $this->t("Numéro de client"),
+      'charge_total' => $this->t("Montant à débourser (Entrer les décimales Ex.: 20.00)"),
+    ];
+
+    $form = [
+      '#type' => 'form',
+      '#attributes' => [
+        'class' => ['col-sm-6'],
+        'action' => 'https://www3.moneris.com/HPPDP/index.php',
+        'method' => 'post',
+        'target' => 'results',
+      ],
+      'ps_store_id' => $psStoreId,
+      'hpp_key' => $hppKey,
+      'lang' => $lang,
+    ];
+
+    foreach ($fields as $name => $label) {
+      $form[$name] = [
+        '#type' => 'textfield',
+        '#title' => $label,
+        '#name' => $name,
+        '#required' => true,
+      ];
+    }
+    $form['note'] = [
+      '#type' => 'checkbox',
+      '#name' => 'note',
+      '#title' => $this->t('Je désire recevoir factures et états de compte par courriel.'),
+      '#default_value' => $this->t("Factures et états de compte par courriel."),
+      '#return_value' => $this->t("Factures et états de compte par courriel."),
+    ];
+
+    $form['actions'] = [
+      '#type' => 'button',
+      '#value' => $this->t("Soumettre"),
+    ];
+
+
+      //$form = BillsPaymentForm::create(\Drupal::getContainer());
+      //$formState = new FormState();
+      //$formState->set('form_is_enabled', isset($config['form_is_enabled']) ? 1 : 0);
 
       $config = \Drupal::config('yqb_bills.settings');
 
@@ -37,8 +102,8 @@ class BillsBlock extends BlockBase implements BlockPluginInterface{
           return [];
       }
 
-      $builtForm = \Drupal::formBuilder()->buildForm($form, $formState);
-      $build['form'] = $builtForm;
+      //$builtForm = \Drupal::formBuilder()->buildForm($form, $formState);
+      $build['form'] = $form;
 
 
     return $build;
