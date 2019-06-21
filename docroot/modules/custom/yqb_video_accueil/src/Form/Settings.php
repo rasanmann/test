38a17d9c38b097ae3e58d73cb4fa2f754ac8cb8c
jcverdier <jean-charles.verdier@libeo.com>
2019-06-21 12:50:40 -0400

    [FEATURE] #95376: Ajout de plusieurs options de configuration. Nettoyage du code.

--- docroot/modules/custom/yqb_video_accueil/src/Form/Settings.php
+++ docroot/modules/custom/yqb_video_accueil/src/Form/Settings.php
@@ -2,11 +2,9 @@
 
 namespace Drupal\yqb_video_accueil\Form;
 
-use Drupal\Console\Bootstrap\Drupal;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\file\Entity\File;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class Settings extends ConfigFormBase {
     const MAX_FILE_SIZE = 15;
@@ -23,15 +21,15 @@ class Settings extends ConfigFormBase {
 
     public function buildForm(array $form, FormStateInterface $form_state) {
         $form = parent::buildForm($form, $form_state);
+        $config = $this->config('yqb_video_accueil.settings');
 
         // La vidéo
         $form['video_accueil_fid'] = array(
             '#type' => 'managed_file',
             '#title' => $this->t('Vidéo'),
             '#description' => t('The uploaded image that will be displayed on the homepage.'),
-            '#upload_location' => 'public://videos',
+            '#upload_location' => $config->get('video_upload_location'),
             '#upload_validators' => array(
-                'sanitizeFilename' => array(),
                 'file_validate_extensions' => array('mp4 webm ogv'),
                 'file_validate_size' => array(self::MAX_FILE_SIZE * 1024 * 1024),
             ),
@@ -42,7 +40,7 @@ class Settings extends ConfigFormBase {
             '#type' => 'managed_file',
             '#title' => $this->t('Poster'),
             '#description' => $this->t('The default image displayed while the video is downloading.'),
-            '#upload_location' => 'public://default_images',
+            '#upload_location' => $config->get('poster_upload_location'),
             '#upload_validators' => array(
                 'file_validate_size' => array(self::MAX_FILE_SIZE * 1024 * 1024),
             ),
@@ -52,9 +50,11 @@ class Settings extends ConfigFormBase {
     }
 
     public function sanitizeFilename(File $file) {
+        // file_move gère les différents cas d'erreur et effectue les modifications dans la base de donnée
         file_move(
             $file,
-            'public://videos/' . preg_replace('/[^A-Za-z0-9]/i', '_', $file->getFilename())
+            $this->config('yqb_video_accueil.settings')->get('video_upload_location') . '/' .
+            preg_replace('/[^A-Za-z0-9\.]/i', '_', $file->getFilename())
         );
     }
 
@@ -66,32 +66,28 @@ class Settings extends ConfigFormBase {
 
     public function submitForm(array &$form, FormStateInterface $form_state) {
         parent::submitForm($form, $form_state);
+        $config = $this->config('yqb_video_accueil.settings');
+        // On ajoute la nouvelle vidéo/image à la config du module
+        // On renomme le fichier pour qu'il soit unix-friendly
+        // On supprime l'ancienne vidéo/image
+        foreach (['video_accueil_fid', 'poster_video_accueil_fid'] as $input) {
+            if (!empty($form_state->getValue($input))) {
+                $oldFile = $config->get($input);
 
-        foreach (['video_accueil_fid', 'poster_video_accueil_fid'] as $config) {
-            if (!empty($form_state->getValue($config))) {
-                $oldFile = $this->config('yqb_video_accueil.settings')->get($config);
-
-                $this->config('yqb_video_accueil.settings')
-                    ->set($config, $form_state->getValue($config));
+		$config->set($input, $form_state->getValue($input));
 
                 $this->sanitizeFilename(
                     File::load(
-                        $this->config('yqb_video_accueil.settings')->get($config)[0]
+                        $config->get($input)[0]
                     )
                 );
 
                 if (isset($oldFile[0])) {
-                    try {
-                        file_delete($oldFile[0]);
-                    } catch(FileException $e) {
-                        // Il se peut que le fichier ait été supprimé manuellement
-                        // L'index $oldFile[0] va exister, mais pas le fichier
-                        // Si tel est le cas, file_delete va planter
-                    }
+                    file_delete($oldFile[0]);
                 }
             }
         }
 
-        $this->config('yqb_video_accueil.settings')->save();
+        $config->save();
     }
 }
