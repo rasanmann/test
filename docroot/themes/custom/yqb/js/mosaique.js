01b7da955dcac0749a795d69074601d1733191b4
jcverdier <jean-charles.verdier@libeo.com>
2019-06-29 13:54:24 -0400

    [FEATURE] #94367: Début de l'ajout du modal avec caroussel.

--- docroot/themes/custom/yqb/js/mosaique.js
+++ docroot/themes/custom/yqb/js/mosaique.js
@@ -1,9 +1,23 @@
 (function ($, Drupal, Isotope) {
   Drupal.behaviors.yqb_mosaique = {
 
+    modalSelector: '#mosaique-modal',
+
     attach: function (context, settings) {
+      this.modal = document.querySelector(this.modalSelector);
+      this.setupIsotope(context);
+      this.setupModal();
+    },
+
+    addListeners: function() {
+      $(document.body).on('click', '.field--name-field-mosaique-image > .field--item', function() {
+
+      });
+    },
+
+    setupIsotope: function(context) {
       $('.field--name-field-mosaique-image', context).isotope({
-        itemSelector: '.field--name-field-mosaique-image > .field--item ',
+        itemSelector: '.field--name-field-mosaique-image > .field--item',
         percentPosition: true,
         layoutMode: 'fitRows',
         masonry: {
@@ -11,6 +25,12 @@
           gutter: 10
         }
       });
+    },
+
+    setupModal: function() {
+      this.addListeners();
     }
+
+
   }
 }(window.jQuery, window.Drupal, window.Isotope));
