ee3d2660c42bb0e162ee6662305dfe6f831122bc
jcverdier <jean-charles.verdier@libeo.com>
2019-07-02 12:20:06 -0400

    [FEATURE] #94367: Le caroussel s'affiche comme un modal.

--- docroot/themes/custom/yqb/js/mosaique.js
+++ docroot/themes/custom/yqb/js/mosaique.js
@@ -1,16 +1,51 @@
-(function ($, Drupal, Isotope) {
+(function ($, Drupal, Isotope, Siema) {
     Drupal.behaviors.yqb_mosaique = {
 
-        carousselSelector: '.siema',
         caroussel: null,
+        selector: {
+            backdrop: '.backdrop',
+            caroussel: '.siema',
+            carousselContainer: '.caroussel-outer',
+            galleryTrigger: '.field--light-gallery'
+        },
 
         attach: function (context, settings) {
             this.setupIsotope(context);
             this.setupCaroussel();
+            this.addListeners();
+        },
+
+        addListeners: function() {
+            var self = this;
+
+            $(document).on('click', this.selector.galleryTrigger, function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                self.setupBackdrop();
+                document.querySelector(self.selector.carousselContainer).classList.add('in');
+            });
+        },
+
+        getItems: function() {
+            var items = Array.prototype.slice.call(document.querySelectorAll('.field--light-gallery > img'));
+
+            return items.map(function(el) {
+                return el.src;
+            });
+        },
+
+        setupBackdrop: function() {
+            if (document.querySelector(this.selector.backdrop) === null) {
+                document.body.insertAdjacentHTML(
+                    'beforeend',
+                    '<div class="backdrop"></div>'
+                );
+            }
+            document.querySelector(this.selector.backdrop).classList.add('in');
         },
 
         setupCaroussel: function () {
-            if (document.querySelector(this.carousselSelector) === null) {
+            if (document.querySelector(this.selector.caroussel) === null) {
                 var list = '';
                 var items = this.getItems();
 
@@ -61,4 +96,4 @@
             });
         }
     }
-}(window.jQuery, window.Drupal, window.Isotope));
+}(window.jQuery, window.Drupal, window.Isotope, window.Siema));
