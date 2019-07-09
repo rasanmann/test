597d8cc2592046651f457edc29d0796dfd9e4710
hlegare <humphray.legare@libeo.com>
2019-07-09 15:00:51 -0400

    [FEATURE]#95114 - Added some logic for saving the user's choice

--- docroot/modules/custom/yqb_alert/src/Form/AlertForm.php
+++ docroot/modules/custom/yqb_alert/src/Form/AlertForm.php
@@ -5,6 +5,7 @@ namespace Drupal\yqb_alert\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 class AlertForm extends FormBase {
 
@@ -22,22 +23,41 @@ class AlertForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state)
   {
-    // TODO: Implement buildForm() method.
+
+    $config = \Drupal::config('yqb_alert.settings');
+
+
 
     $form['french_alert'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('First number'),
+      '#type' => 'textarea',
+      '#title' => $this->t('French Alert'),
+      '#placeholder' => "Entrer le text de l'alerte ici",
+      '#description' => 'description'
     ];
 
     $form['english_alert'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Second number'),
+      '#type' => 'textarea',
+      '#title' => $this->t('English Alert'),
+      '#placeholder' => "Enter the text for the alert",
+      '#description' => 'description'
     ];
+
+    $form['alert_is_enable'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Cocher pour afficher l\'alerte'),
+      '#default_value' => $config->get('yqb_alert.alert_is_enabled'),
+//      '#return_value' => 1
+    ];
+
 
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Calculate'),
+      '#value' => $this->t('Submit'),
     ];
+
+    ksm($config->get('yqb_alert.alert_is_enabled'));
+
+
 
     return $form;
 
@@ -50,7 +70,24 @@ class AlertForm extends FormBase {
 
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
-    drupal_set_message("it works!");
+    $config = \Drupal::config('yqb_alert.settings');
+//    $config->get('alert_is_enabled');
+//    $config = \Drupal::service('yqb_alert.settings')->getEditable('yqb_alert.alert_is_enabled');
+
+
+
+
+//    $config->set('yqb_alert.alert_is_enabled', $form_state->getValue('alert_is_enabled'));
+//    $config->save();
+
+//    drupal_set_message("allo");
+  }
+
+
+  protected function getEditableConfigNames() {
+    return [
+      'yqb_alert.settings',
+    ];
   }
 
 
