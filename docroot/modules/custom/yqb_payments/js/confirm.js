c6101156c5e344957507224a704843a4de1a78d0
Francis Santerre <francis.santerre@gmail.com>
2019-07-11 11:57:52 -0400

    Remove js popup on submit

--- docroot/modules/custom/yqb_payments/js/confirm.js
+++ docroot/modules/custom/yqb_payments/js/confirm.js
@@ -1,8 +1,10 @@
 jQuery(window).bind('beforeunload', function (e) {
+  if(jQuery(e.target.activeElement).is('button[type="submit"]')) return;
   return Drupal.t('Are you sure you want to cancel the current transaction?')
 });
 
 window.addEventListener('beforeunload', function (e) {
+  if(jQuery(e.target.activeElement).is('button[type="submit"]')) return;
   e.preventDefault();
   e.returnValue = Drupal.t('Are you sure you want to cancel the current transaction?');
 });
