46383e204cd1dd74d774dd6a3ca35cc4f46bad00
hlegare <humphray.legare@libeo.com>
2019-07-31 17:43:58 -0400

    [FEATURE]#95114 - added logic so customer can enter alert full in same block and template and js for fixing behavior of link

---
+++ docroot/modules/custom/yqb_alert/src/Controller/ShowAlert.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\yqb_alert\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\block\Entity\Block;
+
+class ShowAlert extends ControllerBase{
+
+
+    public function Index(){
+
+        $block = Block::load('yqbblockalert');
+        $settings = $block->get('settings');
+
+        $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+
+        $language == 'fr' ? $content = $settings['french_alert_full'] : $content = $settings['english_alert_full'];
+
+        if(!$settings['alert_is_enable']){
+            $content = "";
+        }
+
+        return [
+            '#markup' => $content,
+        ];
+        
+    }
+
+}
---
+++ docroot/themes/custom/yqb/js/alerts.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($){
+
+    var masterBlock = $('#block-yqbblockalert');
+
+    //get the block
+    var block = $('#block-yqbblockalert .form-group');
+   
+    //get the block innerhtml
+    var blockText = block.html();
+
+    //get the link
+    var link = $('#block-yqbblockalert .form-group ~ .link');
+
+    //get the link
+    var linkText = $('#block-yqbblockalert .form-group ~ .link').html();
+
+    //add them to the block innerhtml
+    console.log(blockText);
+    console.log(linkText);
+
+    block.html(blockText + " " + linkText);
+    link.remove();
+    
+    masterBlock.css({
+	"display":"block",
+    });
+    
+    
+
+});
