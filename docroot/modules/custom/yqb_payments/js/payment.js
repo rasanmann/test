4887868d97197fff9e284aae6eb887a36d947c94
humphray legare <humphray.legare@libeo.com>
2019-08-15 16:51:51 -0400

    [FIX]

--- docroot/modules/custom/yqb_payments/js/payment.js
+++ docroot/modules/custom/yqb_payments/js/payment.js
@@ -5,72 +5,9 @@
     attach: function (context, settings) {
       $('button :submit').on('click', function(e){
         e.preventDefault();
-        return false;
+        $(e.currentTarget).attr('disabled', true);
       });
     }
   };
+
 })(jQuery, Drupal);
-
-
-
-
-// (function ($, Drupal) {
-//   'use strict';
-//
-//   Drupal.behaviors.yqb_payments = {
-//     attach: function (context, settings) {
-//       $('#edit-submit').on('click', function(e){
-//         e.preventDefault();
-//       });
-//     }
-//   };
-//
-// })(jQuery, Drupal);
-//
-//
-// jQuery(document).ready(function($){
-//   $("#edit-actions").attr("disabled", true);
-//
-//   var monerisDataInput  = false;
-//   var monerisExpInput   = false;
-//   var monerisCVDInput   = false;
-//
-//   function validateInput(e){
-//     var hook = $(e.currentTarget);
-//
-//     if(hook.attr("id") == "monerisDataInput"){
-//       hook.val().match("^[0-9]{16}$") ? monerisDataInput = true : monerisDataInput = false;
-//     }else if (hook.attr("id") == "monerisExpInput"){
-//       hook.val().match("^[0-9]{4}$") ? monerisExpInput = true : monerisExpInput = false;
-//     }else if (hook.attr("id") == "monerisCVDInput"){
-//       hook.val().match("^[0-9]{3}$") ? monerisCVDInput = true : monerisCVDInput = false;
-//     }
-//
-//     if(monerisDataInput && monerisExpInput &&  monerisCVDInput){
-//       $("#edit-actions").attr('disabled', false);
-//     }else {
-//       $("#edit-actions").attr('disabled', true);
-//     }
-//   };
-//
-//   $("#monerisDataInput").on('focusout', function(e){
-//     validateInput(e);
-//   });
-//   $("#monerisExpInput").on('focusout', function(e){
-//     validateInput(e);
-//   });
-//   $("#monerisCVDInput").on('focusout', function(e){
-//     validateInput(e);
-//   });
-//
-//   $("#edit-actions").on("click",function(e){
-//     e.preventDefault();
-//     if(monerisDataInput && monerisExpInput &&  monerisCVDInput){
-//       $(e.currentTarget).attr('disabled', true);
-//       $(e.currentTarget).submit();
-//     }
-//   });
-//
-//
-// });
-//
