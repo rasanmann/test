b857e59d0395a7fed2648fbdcbe38fafbd336c2b
humphray legare <humphray.legare@libeo.com>
2019-10-15 14:03:51 -0400

    modified sync to fgit actuel git url

--- env/sync.sh
+++ env/sync.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
-ssh yqb.dev@staging-25038.prod.hosting.acquia.com -p 22 "/var/www/html/yqb.dev/vendor/drush/drush/drush sql-dump" | docker exec -i yqb_mysql mysql -u yqb -pyqb yqb &> /dev/null
+ssh yqb.prod@ded-25036.prod.hosting.acquia.com -p 22 "/var/www/html/yqb.prod/vendor/drush/drush/drush sql-dump" | docker exec -i yqb_mysql mysql -u yqb -pyqb yqb &> /dev/null
 
 echo -ne "├─ Voulez-vous copier les uploads? [o/N] "
 read copy_uploads
@@ -11,5 +11,5 @@ if [ "$copy_uploads" == "o" -o "$copy_uploads" == "O" ]; then
     if [ ! -f $DIR/../docroot/sites/default/files ]; then
         mkdir $DIR/../docroot/sites/default/files &> /dev/null
     fi
-    rsync -rauve "ssh -p 22" yqb.dev@staging-25038.prod.hosting.acquia.com:/var/www/html/yqbdev/docroot/sites/default/files/* $DIR/../docroot/sites/default/files/. &> /dev/null
+    rsync -rauve "ssh -p 22" yqb.prod@ded-25036.prod.hosting.acquia.com:/var/www/html/yqbprod/docroot/sites/default/files/* $DIR/../docroot/sites/default/files/. &> /dev/null
 fi
