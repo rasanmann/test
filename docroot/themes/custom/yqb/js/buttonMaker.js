d5d69e739a0d156d58c090c1b9a4c208ee50bded
humphray legare <humphray.legare@libeo.com>
2019-12-19 11:57:56 -0500

    [FEATURE]#104449 - added a fix for state hover

--- docroot/themes/custom/yqb/js/buttonMaker.js
+++ docroot/themes/custom/yqb/js/buttonMaker.js
@@ -8,7 +8,22 @@ jQuery(document).ready(function($){
     link.css("border-radius", "10px")
     link.css("color", "white")
     link.css("text-decoration","none")
+
+    link.on("mouseover", function(e){
+      $(e.currentTarget).css({
+        "background": "#6699ff",
+        "cursor": "pointer"
+      });
+    }).on("mouseout", function(e){
+      $(e.currentTarget).css({
+        "background": "#0054ff",
+        "cursor": "pointer"
+      });
+    });
   }
+
+
+
   if(window.location.href.indexOf('/fr/') !== -1) {
     HrefToButtonConverter('https://outlook.office365.com/owa/calendar/BCA1@yqb.onmicrosoft.com/bookings/', 'PRENDRE RENDEZ-VOUS')
   }
