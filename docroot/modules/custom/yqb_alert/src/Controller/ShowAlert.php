ec138d6b5282170de40a2b392c43de2b7126f720
humphray legare <humphray.legare@libeo.com>
2020-01-09 12:21:26 -0500

    [FEATURE]#102204 - fix the showing of full version in no preview

--- docroot/modules/custom/yqb_alert/src/Controller/ShowAlert.php
+++ docroot/modules/custom/yqb_alert/src/Controller/ShowAlert.php
@@ -9,33 +9,30 @@ use Drupal\yqb_blocks\Plugin\Block\PreviewBlock;
 use Drupal\Component\Annotation\Plugin;
 
 
+
+
 class ShowAlert extends ControllerBase{
 
 
     public function Index(){
+
         $block = Block::load('yqbblockalert');
         $settings = $block->get('settings');
-
         $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
-
-
         $store = (Drupal::service('tempstore.private')->get('yqb_blocks_preview'));
-
-        $tempFrenchFull = $store->get('french_alert_full');
-        $tempEnglishFull = $store->get('english_alert_full');
-
 
         // We always erase the tempstore in YqbAlertblock. If any value exist in the tempstore it means were in preview
         if($store->get('french_alert_full')){
+          $tempFrenchFull = $store->get('french_alert_full');
+          $tempEnglishFull = $store->get('english_alert_full');
           $alertEnable = $store->get('alert_is_enable');
           $language == 'fr' ? $content = $tempFrenchFull : $content = $tempEnglishFull;
         }else {
-          $alertEnable = \Drupal::config('yqb_alert_block.settings')->get('alert_is_enable');
-          $language == 'fr' ? $content = $settings['french_alert_full'] : $content = $settings['english_alert_full'];
+          $alertEnable = strval($settings['alert_is_enable']);
+          $language == 'fr' ? $content = strval($settings['french_alert_full']['value']) : $content = strval($settings['english_alert_full']['value']);
         }
 
         if(!$alertEnable){
-          ksm($alertEnable);
           return [
             '#markup' => "",
           ];
