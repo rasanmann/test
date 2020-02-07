1ccd1fb59ba9d29359daf8882c94a8c29efadfd8
humphray legare <humphray.legare@libeo.com>
2020-02-07 09:46:27 -0500

    [FEATURE]#100921 - verify ready document to get video width

--- docroot/themes/custom/yqb/js/mosaique.js
+++ docroot/themes/custom/yqb/js/mosaique.js
@@ -295,13 +295,15 @@
 
         //feature #100921
         fixIframeHeight: function(){
-          $(window).on("resize load",function(){
-            var videoHook = $(".field--item.video-nb > video");
-            var videoWidth = videoHook.width();
-            console.log(videoWidth);
-            var videoHeight = videoHook.height();
-            $(".field--item iframe.media-oembed-content").css({"height":videoHeight, "width": videoWidth});
+          $(document).ready(function(){
+            $(window).on("resize load",function(){
+              var videoHook = $(".field--item.video-nb > video");
+              var videoWidth = videoHook.width();
+              console.log(videoWidth);
+              var videoHeight = videoHook.height();
+              $(".field--item iframe.media-oembed-content").css({"height":videoHeight, "width": videoWidth});
 
+            });
           });
 
         },
