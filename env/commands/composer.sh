235850229a5a308cbb1599b2a4ee3066e0c413fa
Simon <simon.bluteau@libeo.com>
2021-02-03 17:56:25 -0500

    Changement commands

---
+++ env/commands/composer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function composer {
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )/../../"
+ 
+    IMAGE=docker.gitlab.libeo.com/docker/php:7.3-composer
+
+    docker pull ${IMAGE} &> /dev/null
+    docker run --rm -u $(id -u):$(id -g) \
+        -v "${DIR}"/.composer:/.composer \
+        -v "${DIR}":/public \
+        -it ${IMAGE} --working-dir=/public "$@"
+}
