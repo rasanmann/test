3675b88845b8c3784d33bd06acc3105d9d7ad566
Mathieu Quesnel <mathieu.quesnel@libeo.com>
2021-11-23 14:52:22 -0500

    [FIX] Ajustements Kubéo

--- env/command/drush.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-function drush {
-    # TODO: How to manage the fact that this requires MySQL more elegantly
-    docker exec -u root -i yqb_php-fpm apk add mysql-client &> /dev/null
-    docker exec -u root yqb_php-fpm bash -c "mkdir -p /home/app/ && chown app: /home/app" &> /dev/null
-    docker exec -w /app/ -u app -i yqb_php-fpm /app/vendor/drush/drush/drush "$@"
-}
